752797e4620b5ca3e2b658c949d2570a99a1fb58
Mahdi-Mohamadiha <mahdi.mohamadiha.znu@gmail.com>
2025-05-03 12:50:29 +0330

    Set log level to 3

--- screenshot.py
+++ screenshot.py
@@ -23,6 +23,7 @@ def take_screenshot(url, selector, output_path, index=1, timeout=10):
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--log-level=3")
 
     driver = webdriver.Chrome(options=options)
     driver.get(url)
