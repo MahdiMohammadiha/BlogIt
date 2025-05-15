a4d28b342a7f3faca25d7503a10261c78bfd819a
Mahdi-Mohamadiha <mahdi.mohamadiha.znu@gmail.com>
2025-05-15 20:01:04 +0330

    Add market report generation to blog route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, render_template
 from jdatetime import date
+from report_exporter import livetse_market_report, setup_driver
 
 
 app = Flask(__name__)
@@ -12,8 +13,12 @@ def index():
 
 @app.route("/blog")
 def blog():
+    driver = setup_driver()
+    livetse_market_report(driver)
+
     today = date.today()
     jalali_date = today.strftime("%d %B %Y")
+
     return render_template("blog.html", jalali_date=jalali_date)
 
 
