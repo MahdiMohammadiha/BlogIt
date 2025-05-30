624b7b5f2ae940e4a98592510dbcf6a38cda136a
Mahdi-Mohamadiha <mahdi.mohamadiha.znu@gmail.com>
2025-05-30 13:16:49 +0330

    Refactor blog function: update date handling to use dynamic paths for report templates and improve variable naming

--- app.py
+++ app.py
@@ -23,21 +23,25 @@ def blog():
     report_exporter_main()
     batch_runner_main()
 
+    pretty_jdate = str(JalaliDate().pretty())
+    iso_jdate = str(JalaliDate().iso())
+
     golden_notification = not is_file_empty(
-        "templates/reports/livetse_golden_notification_report.html"
+        f"templates/reports/{iso_jdate}/livetse_golden_notification_report.html"
     )
 
     blog_media_path = load_config("blog_media_path.json")
-    tsetmc_index_report = load_config("templates/reports/tsetmc_index_report.json")
-
-    jalali_date = JalaliDate().pretty()
+    tsetmc_index_report = load_config(
+        f"templates/reports/{iso_jdate}/tsetmc_index_report.json"
+    )
 
     return render_template(
         "blog.min.html",
-        jalali_date=jalali_date,
+        iso_jdate=iso_jdate,
+        pretty_jdate=pretty_jdate,
         golden_notification=golden_notification,
         tsetmc_index_report=tsetmc_index_report,
-        **blog_media_path
+        **blog_media_path,
     )
 
 
